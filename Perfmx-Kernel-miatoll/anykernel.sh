1bd8d600d705460e48bd82100f388480430038ee
Swasthik Moolya <swasthikmoolya14@gmail.com>
2023-02-07 21:24:12 +0530

    kernels

---
+++ Perfmx-Kernel-miatoll/anykernel.sh
@@ -0,0 +1,62 @@
+# Swasthik's Ramdisk Mod Script
+# Kernels @ Swasthik_Moolya
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Nano Kernel
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=miatoll
+device.name2=curtana
+device.name3=gram
+device.name4=excalibur
+device.name5=joyeuse
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=auto;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 750 750 $ramdisk/*;
+
+## AnyKernel install
+dump_boot;
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+
+write_boot;
+## end boot install
+
+
+# shell variables
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+
+## AnyKernel vendor_boot install
+#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+
+#flash_boot;
+## end vendor_boot install
